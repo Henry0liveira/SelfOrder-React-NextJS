8ad41255cd488d19f2d2b5ba999e6ae9a3bce9a9
Henry Thomas da Silva de Oliveira <henry.t.oliveira6@aluno.senai.br>
2025-10-28 23:41:32 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's2-receitas.glbimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
