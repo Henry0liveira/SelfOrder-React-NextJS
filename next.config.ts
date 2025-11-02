404c6079b42927837b10445a4c97f48084966fbc
Henry Thomas da Silva de Oliveira <henry.t.oliveira6@aluno.senai.br>
2025-11-02 12:57:57 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'static1.squarespace.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
